d794d44472c7eb7d5dfbb4b2d82c9fba96bdb735
QDarius <paul.hebert@paulhebertdesigns.com>
2016-02-27 16:54:07 -0800

    Began Support page. Began formatting resources

--- resources/index.php
+++ resources/index.php
@@ -4,27 +4,29 @@
 	include('../includes/page_sections/header.php'); 
 ?>
 		<div class="main_content">
-			<h1>Resources</h1>
-			<section>
-				<h2>Email Coding</h2>
-				<ul>
-					<li><a href="https://www.campaignmonitor.com/" target="_BLANK">Campaign Monitor</a></li>				
-					<li><a href="https://www.campaignmonitor.com/css/" target="_BLANK">Campaign Monitor's Ultimate Guide to CSS</a></li>
-					<li><a href="http://templates.mailchimp.com/resources/inline-css/" target="_BLANK">Mailchimp's CSS inliner</a></li>
-				</ul>
-			</section>
-			<section>
-				<h2>Email Testing</h2>
-				<ul>
-					<li><a href="https://litmus.com/" target="_BLANK">Litmus</a></li>
-				</ul>
-			</section>
-			<section>
-				<h2>Email Delivery</h2>
-				<ul>			
-					<li><a href="http://mailchimp.com/" target="_BLANK">Mailchimp</a></li>
-				</ul>
-			</section>
+			<div class="col-lg-6 col-lg-offset-3">      
+				<h1>Resources</h1>
+				<section>
+					<h2>Email Coding</h2>
+					<ul>
+						<li><a href="https://www.campaignmonitor.com/" target="_BLANK">Campaign Monitor</a></li>				
+						<li><a href="https://www.campaignmonitor.com/css/" target="_BLANK">Campaign Monitor's Ultimate Guide to CSS</a></li>
+						<li><a href="http://templates.mailchimp.com/resources/inline-css/" target="_BLANK">Mailchimp's CSS inliner</a></li>
+					</ul>
+				</section>
+				<section>
+					<h2>Email Testing</h2>
+					<ul>
+						<li><a href="https://litmus.com/" target="_BLANK">Litmus</a></li>
+					</ul>
+				</section>
+				<section>
+					<h2>Email Delivery</h2>
+					<ul>			
+						<li><a href="http://mailchimp.com/" target="_BLANK">Mailchimp</a></li>
+					</ul>
+				</section>
+			</div>
 		</div>
 	</body>
 </html>
--- support/index.php
+++ support/index.php
@@ -4,7 +4,9 @@
 	include('../includes/page_sections/header.php'); 
 ?>
 		<div class="main_content">
-
+      		<div class="col-lg-6 col-lg-offset-3">  
+      			<h1>Submit your Support ticket</h1>    
+      		</div>
 		</div>
 	</body>
 </html>
