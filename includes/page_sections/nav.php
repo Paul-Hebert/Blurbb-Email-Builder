ccef5652455b929584c8a1cb884cf41bfc4a195d
QDarius <paul.hebert@paulhebertdesigns.com>
2016-02-28 23:55:09 -0800

    Began responsifying

--- includes/page_sections/nav.php
+++ includes/page_sections/nav.php
@@ -1,7 +1,10 @@
 <?php
-			echo '<nav>
+			echo '<nav class="hidden-xs">
            		<a href="' . $base_url . 'builder">Builder</a>
                 <a href="' . $base_url . 'resources/">Resources</a>
         		<a href="' . $base_url . 'support/">Support</a>
         	</nav>';
 ?>
+			<nav class="visible-xs">
+				<i class="fa fa-bars"></i>&nbsp;&nbsp;Menu
+			</nav>
