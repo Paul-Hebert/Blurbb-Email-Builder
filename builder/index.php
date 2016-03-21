1619d8223dbca67dde78118d65f09940069851a5
QDarius <paul.hebert@paulhebertdesigns.com>
2016-03-20 20:56:05 -0700

    Split theme css

--- builder/index.php
+++ builder/index.php
@@ -1,18 +1,18 @@
-    <?php
-      $title = 'Theme Selection';
-      $base_url = '../';
+          <?php
+               $title = 'Theme Selection';
+               $base_url = '../';
 
-      include('../includes/page_sections/header.php');
-    ?>
-    <div class="main_content">
-      <div class="col-lg-6 col-lg-offset-3">
-        <h1>Ready to build your e-mail?</h1>
-        <?php include('dashboard/theme_picker.php'); ?>
-        <button id="theme_selection_button" class="col-lg-12">Pick a Theme</button>
-      </div>
-    </div>
-    <?php
-      include($base_url . 'includes/page_sections/scripts.php');
-    ?>
-  </body>
+               include('../includes/page_sections/header.php');
+          ?>
+          <div class="main_content">
+               <div class="col-lg-6 col-lg-offset-3">
+                    <h1>Ready to build your e-mail?</h1>
+                    <?php include('dashboard/theme_picker.php'); ?>
+                    <button id="theme_selection_button" class="col-lg-12">Pick a Theme</button>
+               </div>
+          </div>
+          <?php
+               include($base_url . 'includes/page_sections/scripts.php');
+          ?>
+     </body>
 </html>
