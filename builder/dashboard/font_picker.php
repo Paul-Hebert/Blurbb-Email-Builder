d5f40213b4fd864ed1419c59eafdbd515a6a8820
QDarius <paul.hebert@paulhebertdesigns.com>
2016-03-30 22:56:07 -0700

    Switched logo and fonts. Standardized sizing and animation with scss variables

--- builder/dashboard/font_picker.php
+++ builder/dashboard/font_picker.php
@@ -1,9 +1,7 @@
         <div class="subsection">
           <label for="font">Web Safe Fonts 
             <span class="icon_toggle" data-heading="Web Safe Fonts">
-              <span class="icon">
-                ?
-              </span>
+              <i class="fa fa-info-circle"></i>
               <div class="toggled_text">
                 <p>Web safe fonts have the best chance of displaying correctly for the email's recipient.</p>
                 <a href="http://www.fonts.com/content/learning/fyti/using-type-tools/fonts-on-the-web" target="_BLANK">Read More About Web Safe Fonts</a>
