da35bbe056b8f48c2dc04aaae09fa577ccfac3f5
QDarius <paul.hebert@paulhebertdesigns.com>
2016-04-10 15:41:19 -0700

    Simplified text_styles JS

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -245,6 +245,9 @@ function initialize_text_pickers(){
 }
 
 function initialize_text_styles(){
+	var target = $('.text_picker, .header_picker, .list_picker').attr('data_target');
+
+
 	$('.alignment i').click(function(){
 		$(this).parent().children('.alignment i').removeClass('selected');
 
@@ -254,16 +257,16 @@ function initialize_text_styles(){
 
 		$(this).addClass('selected');		
 
-		$('#added_CSS').append('#email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ', #email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ' *{text-align:' + alignment + '}');
+		$('#added_CSS').append('#email ' + target + ', #email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ' *{text-align:' + alignment + '}');
 	});
 
 	$('.fa-bold').click(function(){
 		$(this).toggleClass('selected');
 
 		if( $(this).hasClass('selected') ){
-			$('#added_CSS').append('#email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ', #email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ' *{font-weight:bold}');			
+			$('#added_CSS').append('#email ' + target + ', #email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ' *{font-weight:bold}');			
 		}	else{
-			$('#added_CSS').append('#email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ', #email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ' *{font-weight:normal}');						
+			$('#added_CSS').append('#email ' + target + ', #email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ' *{font-weight:normal}');						
 		}
 	});
 
@@ -271,9 +274,9 @@ function initialize_text_styles(){
 		$(this).toggleClass('selected');
 
 		if( $(this).hasClass('selected') ){
-			$('#added_CSS').append('#email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ', #email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ' *{font-style:italic}');			
+			$('#added_CSS').append('#email ' + target + ', #email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ' *{font-style:italic}');			
 		}	else{
-			$('#added_CSS').append('#email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ', #email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ' *{font-style:normal}');						
+			$('#added_CSS').append('#email ' + target + ', #email ' + $('.text_picker, .header_picker, .list_picker').attr('data_target') + ' *{font-style:normal}');						
 		}	
 	});
 }
