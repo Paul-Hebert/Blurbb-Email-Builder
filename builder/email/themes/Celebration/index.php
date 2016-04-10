c93d3bbb9b44a378ba35a55a32496b5f12ef48b5
QDarius <paul.hebert@paulhebertdesigns.com>
2016-04-10 12:19:52 -0700

    More improvements to the Celebration theme

--- builder/email/themes/Celebration/index.php
+++ builder/email/themes/Celebration/index.php
@@ -1,5 +1,6 @@
 <?php
   start_email();  
+
     start_row('style1');      
       start_column('style1'); 
         append_block('header', 1, 'A Holiday Celebration');        
@@ -15,8 +16,35 @@
     start_row('style2');
       start_column('style2'); 
         append_block('header', 2, 'You&rsquo;re Invited!');      
-        append_block('text', 1, 'An e-commerce site selling high quality, hand decorated skateboard decks. Board in Hand sells decks as art pieces as well as complete decks that are ready to ride!');        
+        append_block('text', 1, 'Come over this Sunday for a day of friends, family and fun!');        
       end_column();                   
-    end_row();       
+    end_row();   
+
+
+    start_row('style2');
+      start_column('style2'); 
+        append_block('image', 2, 'email/themes/Celebration/imgs/map.png');        
+      end_column();                   
+    end_row();   
+
+
+    start_row('style1');
+      start_column('style1'); 
+        append_block('text', 2, 'Bla');        
+      end_column();  
+      start_column('style1'); 
+        append_block('text', 3, 'Bla');        
+      end_column();   
+      start_column('style1'); 
+        append_block('text', 4, 'Bla');        
+      end_column();                    
+    end_row();
+
+    start_row('style2');
+      start_column('style2'); 
+        append_block('text', 5, 'Come over this Sunday for a day of friends, family and fun!');        
+      end_column();                   
+    end_row();   
+
   end_email();
 ?>
